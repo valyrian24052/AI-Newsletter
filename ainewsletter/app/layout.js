6168ebf50edf0cb27b5124b0bcc8db97dd5914c0
Shashank Sharma <79395827+valyrian24052@users.noreply.github.com>
2024-09-02 03:43:00 +0530

    aaa

--- ainewsletter/app/layout.js
+++ ainewsletter/app/layout.js
@@ -5,7 +5,7 @@ import Navbar from './components/navbar';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Curio",
+  title: "Driftio",
   description: "AI Newsletter",
 };
 
